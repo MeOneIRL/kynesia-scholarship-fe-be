7c042d98b969ff0d101bc1b86e135f34d1bfc93e
Muhammad Fahmi Alwan <fahmi.alwan@gmail.com>
2021-12-14 12:50:35 +0700

    Update link headerLandingPage

--- resources/views/layouts/headerLandingPage.blade.php
+++ resources/views/layouts/headerLandingPage.blade.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container mx-auto grid justify-items-center">
-        <a href="/home">
+        <a href="/">
             <img class="my-4 mx-1 md:my-8 md:mx-2.5 h-auto w-32 md:w-60 " src="{{ asset('images/logo.svg')}}" alt="">
         </a>
     </div>
