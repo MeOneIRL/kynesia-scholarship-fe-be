61054e302853103acb7f1399e485620c647310ea
Muhammad Fahmi Alwan <fahmi.alwan@gmail.com>
2021-12-15 18:13:22 +0700

    edit flash message style

--- resources/views/pendaftaran/masukPendaftaran.blade.php
+++ resources/views/pendaftaran/masukPendaftaran.blade.php
@@ -7,7 +7,9 @@
 @include('layouts.headerLandingPage')
 
 <section>
-    <div class="message">{{Session::get('message')}}</div>
+    <div class="max-w-screen-sm mx-auto">
+        @include('layouts.sessionFlashMessage')
+    </div>
     <div class="container mx-auto px-8 md:px-28 py-12 max-w-screen-sm border-0 md:border border-gray-300 rounded-md">
         <h3 class="mb-8 md:mb-12 text-2xl md:text-3xl text-secondary-color text-center">
             Masuk Akun Pendaftaran
