a43fdc05aee162c75f2cd9fb6ebf39a4a173b258
Muhammad Fahmi Alwan <fahmi.alwan@gmail.com>
2021-12-16 21:06:26 +0700

    import Iodine.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,13 @@
-import Alpine from 'alpinejs'
+import Alpine from 'alpinejs';
+import { Iodine } from '@kingshott/iodine';
+
+const iodine = new Iodine();
 
 window.Alpine = Alpine
 
 Alpine.start()
 
 require('./bootstrap');
+
 
 
